257b0d9660e8ecc960a8d8147b7e93f8df70250a
Juan Navarro <juanavarroquintero@gmail.com>
2022-03-16 14:29:48 -0500

    Cloud final

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -1,6 +1,14 @@
 package duke.choice;
 
 import java.util.Arrays;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerConfiguration;
+import io.helidon.webserver.WebServer;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class ShopApp {
 
@@ -24,6 +32,22 @@ public class ShopApp {
 		Clothing item3 = new Clothing("Green Scarf", 5, "S");
 		Clothing item4 = new Clothing("Blue T-Shirt", 10.5, "S");
 		Clothing[] items = {item1, item2, item3, item4};
+		
+		try {
+			ItemList list = new ItemList(items);
+			
+			Routing routing = Routing.builder().get("/items", list).build();
+			
+			ServerConfiguration config = ServerConfiguration.builder().bindAddress(InetAddress.getLocalHost()).port(8888).build();
+			
+			WebServer ws = WebServer.create(config, routing);
+			
+			ws.start();
+			
+		}catch(UnknownHostException ex) {
+			ex.printStackTrace();
+		}
+		
 		
 		
 		//final double tax = 0.2;
